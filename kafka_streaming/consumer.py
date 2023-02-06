b980b9c0faea6196edef0a5649975d795e9f7557
Acsts <antoine.costes1@gmail.com>
2023-02-06 17:59:05 +0100

    Create Kafka consumer and cutom image to use it

--- kafka_streaming/consumer.py
+++ kafka_streaming/consumer.py
@@ -4,26 +4,26 @@ import ccloud_lib
 import time
 import pandas as pd
 import os
+import pickle
 
+# Kafka config
 CONF = ccloud_lib.read_ccloud_config("python.config")
 TOPIC = "netflix_recommandation" 
 consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(CONF)
-consumer_conf['group.id'] = 'my_tesla_stock_watcher'
+consumer_conf['group.id'] = 'netflix_reco'
 consumer_conf['auto.offset.reset'] = 'latest'
 consumer = Consumer(consumer_conf)
 consumer.subscribe([TOPIC])
 
-# Load global df in csv format in order to store values
-namefile = "local_data_storage.csv"
-if namefile in os.listdir('.'):
-    df = pd.read_csv(namefile, sep=";", names = ["datetime","current_price"])
-else :
-    df = pd.DataFrame(columns = ["datetime","current_price"])
-    df.to_csv(namefile,header = False, index = False)
+# Get title-id mapping for all movies
+df_title = pd.read_csv('movie_titles.csv', encoding = "ISO-8859-1", header = None, names = ['Movie_Id', 'Year', 'Name'], on_bad_lines='skip')
+df_title.set_index('Movie_Id', inplace = True)
+
+# load recommendation model
+svd = pickle.load(open('model_p.sav', 'rb'))
+
 try:
     while True:
-        # With direct transmission 
-        """
         msg = consumer.poll(1.0)
         if msg is None:
             print("Waiting for message or event/error in poll()")
@@ -33,37 +33,16 @@ try:
         else:
             record_key = msg.key()
             record_value = msg.value()
-            data = json.loads(record_value)
-            print(data)
-            print(f"At {data['time_of_price']}, the Tesla stock price is {data['current_price']}$")
+            model_input = json.loads(record_value) # input to feed to model : a user ID
+            # Display the 10 most recommended movies for user :
+            user_pred = df_title.copy() 
+            user_pred = user_pred.reset_index()
+            user_pred['Estimate_Score'] = user_pred['Movie_Id'].apply(lambda x: svd.predict(model_input, x).est)
+            user_pred = user_pred.sort_values('Estimate_Score', ascending=False)
+            print(user_pred.reset_index().drop(columns=['Year', 'Movie_Id', 'index']).head(10))
             time.sleep(1)
-        """
-        # With waiting 10 messages before saving
-        print("Collecting 5 new messages")
-        msg = consumer.consume(num_messages=5,timeout=10)
-        print("Messages collected")
-        if msg is None:
-            # No message available within timeout.
-            # Initial message consumption may take up to
-            # `session.timeout.ms` for the consumer group to
-            # rebalance and start consuming
-            print("Waiting for message or event/error in poll()")
-            continue
-        else:
-            # Useless to print message because non readable 
-            # print("### Un-processed msg")
-            # print(msg)
-            print("### Value msg")
-            h_msg = [json.loads(x.value()) for x in msg]
-            df_temp = pd.DataFrame.from_dict(h_msg)
-            print(df_temp)
-            print("Adding those row to global dataframe")
-            df = pd.concat([df,df_temp], axis=0)
             
 except KeyboardInterrupt:
-    # Saving all data before exiting
-    df.to_csv(namefile,header = False, index = False)
-    print("Success for saving data")
     pass
 finally:
     consumer.close()
